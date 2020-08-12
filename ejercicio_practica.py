c6b9f1f63e33ba16f284b0b40c0cdf3a011121ce
Henrry <64995880+henrryyanez20@users.noreply.github.com>
2020-08-12 16:50:44 -0300

    Ya listo el scheme y el import csv

---
+++ ejercicio_practica.py
@@ -0,0 +1,51 @@
+import csv, sqlite3
+
+def create_schema():
+    conn = sqlite3.connect('libreria.db')
+    c = conn.cursor()
+    c.execute("""
+                DROP TABLE IF EXISTS t;
+            """)
+
+    c.execute("""
+                CREATE TABLE t (
+                    [titulo] TEXT NOT NULL, 
+                    [cantidad_paginas] INTEGER, 
+                    [autor] TEXT
+                );""")
+    conn.commit()
+    conn.close()
+
+
+def fill():
+    conn = sqlite3.connect('libreria.db')
+    c = conn.cursor()
+    
+    with open('libreria.csv', 'r') as fin:
+        dr = csv.DictReader(fin)
+        to_db = [(i['titulo'], i['cantidad_paginas'], i['autor']) for i in dr]
+
+    c.executemany("""
+            INSERT INTO t (titulo, cantidad_paginas, autor)
+            VALUES (?,?,?);""", to_db)
+    conn.commit()
+    conn.close()
+
+
+
+
+if __name__ == "__main__":
+  # Crear DB
+  create_schema()
+
+  # Completar la DB con el CSV
+  fill()
+
+  # Leer filas
+  #fetch()  # Ver todo el contenido de la DB
+  #fetch(3)  # Ver la fila 3
+  #fetch(20)  # Ver la fila 20
+
+  # Buscar autor
+  #print(search_author('Relato de un naufrago'))
+
